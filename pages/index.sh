ef3e575ded35380acc2bc041aa0e114a66b887eb
Tenet <40796508+luisadha@users.noreply.github.com>
2025-09-14 18:05:12 +0700

    Update index.sh

--- pages/index.sh
+++ pages/index.sh
@@ -5,21 +5,7 @@ end_headers
 title="Toko Luis Topup Robux"
 htmx_page <<EOF
 <header class="hero">
-<script src="https://cdn.tailwindcss.com"></script>
-  <style>
-    /* animasi custom untuk fade */
-    .fade-out {
-      opacity: 0;
-      transition: opacity 1s ease;
-    }
-    .zoom-in {
-      animation: zoomIn 1.5s ease;
-    }
-    @keyframes zoomIn {
-      from { transform: scale(0.5); opacity: 0; }
-      to { transform: scale(1); opacity: 1; }
-    }
-  </style>
+
 </head>
 <body class="h-screen w-screen overflow-hidden font-sans">
 
@@ -43,22 +29,6 @@ Roblox Corporation</p>
     </a>
   </div>
 
-  <!-- Sambutan transparan di atas -->
-  <div id="welcome" class="absolute inset-0 bg-black/60 flex flex-col items-center justify-center text-white z-10">
-    <img src="./static/images/roblox-blue.png" alt="Logo Website" class="w-36 h-auto zoom-in">
-    <h1 class="text-3xl font-bold mt-6 animate-fadeIn">Roblox Gift Card (IDR RBX)</h1>
-    <h2 class="text-xl mt-2 animate-fadeIn">Roblox Corporation</h2>
-  </div>
 
-  <script>
-    // Hilangkan sambutan setelah 3 detik
-    setTimeout(() => {
-      const welcome = document.getElementById("welcome");
-      welcome.classList.add("fade-out");
-      setTimeout(() => {
-        welcome.style.display = "none";
-      }, 1000); // delay untuk transisi
-    }, 3000);
-  </script>
 </header>
 EOF
