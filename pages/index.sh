5c5306573ee55e4c53b69b9f4bf06135781a6521
Tenet <40796508+luisadha@users.noreply.github.com>
2025-09-14 17:11:57 +0700

    Update index.sh

--- pages/index.sh
+++ pages/index.sh
@@ -25,14 +25,14 @@ htmx_page <<EOF
 
   <!-- Halaman utama -->
   <div id="main" class="absolute inset-0 bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center text-gray-800"
-       style="background-image: url('./assets/images/background.jpg'); z-index: 1;">
+       style="background-image: url('./static/images/background.jpg'); z-index: 1;">
     <h1 class="text-4xl font-bold mb-4">Topup-roblox</h1>
     <p class="text-lg"><a href="/produk">Lihat semua paket</a></p>
   </div>
 
   <!-- Sambutan transparan di atas -->
   <div id="welcome" class="absolute inset-0 bg-black/60 flex flex-col items-center justify-center text-white z-10">
-    <img src="./assets/images/roblox-blue.png" alt="Logo Website" class="w-36 h-auto zoom-in">
+    <img src="./static/images/roblox-blue.png" alt="Logo Website" class="w-36 h-auto zoom-in">
     <h1 class="text-3xl font-bold mt-6 animate-fadeIn">Roblox Gift Card (IDR RBX)</h1>
     <h2 class="text-xl mt-2 animate-fadeIn">Roblox Corporation</h2>
   </div>
