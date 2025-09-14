143c461c586fdbf545ac0a0edc9008dc529e256f
Tenet <40796508+luisadha@users.noreply.github.com>
2025-09-14 12:46:21 +0700

    Update index.sh

--- pages/index.sh
+++ pages/index.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 # index.sh — homepage
-cat <<'HTML'
-Content-Type: text/html; charset=utf-8
+# Content-Type: text/html; charset=utf-8
 
+cat <<'HTML'
 <!doctype html>
 <html lang="id">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
-  <title>Toko Robux (Bash Stack)</title>
+<title>Toko Luis Topup Robux </title>
   <link rel="stylesheet" href="/static/style.css">
 </head>
 <body>
   <header class="hero">
-    <h1>Toko Robux (Bash Stack)</h1>
-    <p>Cara cepat & simpel jual Robux (contoh demo).</p>
+    <h1>Beli Robux</h1>
+    <p>Murah & Cepat | Jual Robux Harga  Miring</p>
     <a href="/produk">Lihat Paket</a>
   </header>
 </body>
