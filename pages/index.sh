516d6d218d6e0d4e3488854dd9424266a9f84879
Tenet <40796508+luisadha@users.noreply.github.com>
2025-09-14 17:55:08 +0700

    Update index.sh

--- pages/index.sh
+++ pages/index.sh
@@ -34,11 +34,12 @@ htmx_page <<EOF
     <h1 class="text-4xl font-bold mb-8">Voucher</h1>
 
     <!-- Card kategori -->
-    <a href="/produk" 
+    <a href="product/voucher/roblox" 
        class="block bg-white/90 backdrop-blur-sm rounded-2xl shadow-lg hover:shadow-xl transform hover:scale-105 transition p-6 w-64 text-center">
       <img src="./static/images/roblox-blue.png" alt="Roblox Games" class="w-20 mx-auto mb-4">
-      <h2 class="text-lg font-semibold text-gray-800">Roblox Games</h2>
-      <p class="text-sm text-gray-600 mt-2">Lihat semua paket topup</p>
+      <h2 class="text-lg font-semibold text-gray-800">Roblox Voucher</h2>
+      <p class="text-sm text-gray-600 mt-2">Roblox Gift Card (IDR RBX)
+Roblox Corporation</p>
     </a>
   </div>
 
