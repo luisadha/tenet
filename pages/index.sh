db11387c3b0b51c2ba3e08dad70213fe851539a6
Tenet <40796508+luisadha@users.noreply.github.com>
2025-10-06 07:33:53 +0700

    Update index.sh

--- pages/index.sh
+++ pages/index.sh
@@ -18,6 +18,16 @@ htmx_page <<EOF
 
   <h1 class="text-4xl sm:text-5xl font-bold mt-8 mb-6">Populer</h1>
   <h1 class="text-4xl sm:text-5xl font-bold mb-6">Games</h1>
+
+ <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 w-full max-w-3xl">
+  <a href="/product/diamond/mlbb"
+       class="block bg-white/90 backdrop-blur-sm rounded-3xl shadow-lg hover:shadow-xl transform hover:scale-105 transition p-6 w-full text-center">
+      <img src="/static/images/mlbb.png" alt="MLBB" class="w-24 mx-auto mb-5">
+      <h2 class="text-xl font-semibold text-gray-800">Mobile Legends</h2>
+      <p class="text-base text-gray-600 mt-2">Diamond & Top-Up Resmi Moonton</p>
+    </a>
+  </div> 
+  
   <h1 class="text-4xl sm:text-5xl font-bold mb-10">Voucher</h1>
 
   <!-- Card kategori -->
@@ -29,13 +39,13 @@ htmx_page <<EOF
       <p class="text-base text-gray-600 mt-2">Roblox Gift Card (IDR RBX) Roblox Corporation</p>
     </a>
 
-    <!-- Placeholder untuk game berikutnya -->
-    <a href="/product/voucher/mlbb"
+    <!-- Placeholder untuk game berikutnya 
+    <a href="/product/diamond/mlbb"
        class="block bg-white/90 backdrop-blur-sm rounded-3xl shadow-lg hover:shadow-xl transform hover:scale-105 transition p-6 w-full text-center">
       <img src="/static/images/mlbb.png" alt="MLBB" class="w-24 mx-auto mb-5">
       <h2 class="text-xl font-semibold text-gray-800">Mobile Legends</h2>
       <p class="text-base text-gray-600 mt-2">Diamond & Top-Up Resmi Moonton</p>
-    </a>
+    </a> -->
   </div>
 
   <div class="h-20"></div> <!-- Spacer agar tidak terlalu nempel di bawah -->
